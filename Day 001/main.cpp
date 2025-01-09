015b05bde07693a10d30cd70fede7652c44b442b
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-01-09 22:41:15 +0530

    Add a file by upload

--- Day 001/main.cpp
+++ Day 001/main.cpp
@@ -1,35 +1,23 @@
-#include<iostream>
-#include<climits>
+/*
+    Title: Find Second Largest Element in an Array
+    Description:
+        This program finds the second largest element in an array of integers. 
+        It iterates through the array, keeping track of the largest and second 
+        largest elements. If the array has fewer than two elements, it returns -1.
+
+    Input:
+        - Size of the array (n)
+        - Array of integers (n elements)
+
+    Output:
+        - The second largest element in the array, or -1 if not applicable.
+
+    Author: Vishal
+    Date: [Insert Date Here]
+*/
+
+#include <iostream>
+#include <climits>
 using namespace std;
 
-int second_largest(int arr[],int n){
-    if(n<2){
-        return -1;
-    }
-    
-    int largest=INT_MIN;
-    int second_largest_element=INT_MIN;
-    for(int i = 0; i<n; i++){
-        if(arr[i]>largest){
-            second_largest_element=largest;
-            largest=arr[i];
-        }
-        else if(arr[i]>second_largest_element && arr[i]!=largest){
-            second_largest_element=arr[i];
-        }
-    }
-    return second_largest_element == INT_MIN ? -1 : second_largest_element;
-}
-
-int main(){
-    int n;
-    cout<<"Enter the value of n : ";
-    cin>>n;
-    int arr[n];
-    cout<<"Enter the array elements : ";
-    for (int i = 0; i < n; i++) {
-        cin>>arr[i];
-    }
-    cout<<second_largest(arr,n);
-    return 0;
-}
+// Function to find the second largest element in the array
