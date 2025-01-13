cdb069bb98e2ace8328fbdc99274eec71c70c226
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-01-13 23:58:47 +0530

    main.cpp

--- Day 011/main.cpp
+++ Day 011/main.cpp
@@ -21,7 +21,6 @@ int maxSubArraySum(int arr[], int n) {
 int main() {
     int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     int n = sizeof(arr) / sizeof(arr[0]); 
-
     int result = maxSubArraySum(arr, n);
     
     cout << "Maximum subarray sum is: " << result << endl;
