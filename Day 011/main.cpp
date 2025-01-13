688484cac53f657df1af2e681c64e3f731bc59ae
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-01-13 23:55:59 +0530

    Add files via upload

---
+++ Day 011/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <climits> // 
+using namespace std;
+
+int maxSubArraySum(int arr[], int n) {
+    int maxSum = INT_MIN; 
+    
+    int currentSum = 0;
+
+    for (int i = 0; i < n; ++i) {
+        currentSum += arr[i];
+        if (currentSum > maxSum) {
+            maxSum = currentSum; 
+            
+        }
+        if (currentSum < 0) {
+            currentSum = 0; 
+            
+        }
+    }
+
+    return maxSum;
+}
+
+int main() {
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]); 
+
+    int result = maxSubArraySum(arr, n);
+    
+    cout << "Maximum subarray sum is: " << result << endl;
+
+    return 0;
+}
