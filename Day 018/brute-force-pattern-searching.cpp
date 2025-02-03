d3f74ead72a4155230a54eb0577f86fb9d15adda
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-02-03 23:15:48 +0430

    Update brute-force-pattern-searching.cpp

--- Day 018/brute-force-pattern-searching.cpp
+++ Day 018/brute-force-pattern-searching.cpp
@@ -1,5 +1,6 @@
 /*
-    Title: Brute Force String Searching Algorithm
+    Title: 
+        Brute Force String Searching Algorithm
     Description:
         - This program searches for a pattern in a given text using 
           the brute force approach.
