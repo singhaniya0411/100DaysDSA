787f3e3152e17194e3d89c37a6623a927b6b98d7
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-03-03 23:32:10 +0530

    Update binary-search-algo.cpp

--- Day 034/binary-search-algo.cpp
+++ Day 034/binary-search-algo.cpp
@@ -15,7 +15,7 @@
     Time Complexity: **O(N log M)** (binary search over `M = sum(arr) - max(arr)`).
     Space Complexity: **O(1)** (No extra space used).
 
-    Author: Vishal
+    Author: Vishal Singhaniya
 */
 
 #include <iostream>
@@ -23,15 +23,6 @@
 #include <numeric>    // accumulate
 using namespace std;
 
-/**
- * @brief Checks if we can assign books such that no student gets more than 'pageLimit' pages.
- * 
- * @param arr Array of books with pages.
- * @param k Number of students.
- * @param pageLimit Maximum pages allowed per student.
- * @param size Number of books.
- * @return true if books can be allocated within the limit, false otherwise.
- */
 bool check(int arr[], int k, int pageLimit, int size) {
     int count = 1;  // Number of students assigned
     int pageSum = 0;
@@ -51,14 +42,7 @@ bool check(int arr[], int k, int pageLimit, int size) {
     return true;
 }
 
-/**
- * @brief Finds the minimum maximum pages a student has to read (Binary Search Approach).
- * 
- * @param arr Array of books with pages.
- * @param size Number of books.
- * @param k Number of students.
- * @return Minimum possible maximum pages.
- */
+
 int findPages(int arr[], int size, int k) {
     if (k > size) return -1; // If students > books, return impossible case
 
