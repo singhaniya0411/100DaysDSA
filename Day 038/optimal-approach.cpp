a98e1441109e3bfd7d24e0979f0d30a31f15a328
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-03-11 22:51:41 +0530

    Update optimal-approach.cpp

--- Day 038/optimal-approach.cpp
+++ Day 038/optimal-approach.cpp
@@ -1,5 +1,6 @@
 /*
-    Title: Search an Element in a Row-wise and Column-wise Sorted Matrix
+    Title:
+        Search an Element in a Row-wise and Column-wise Sorted Matrix
     
     Problem Statement:
         - Implement a function to search for an element in an M x N matrix 
