d91728055409ebf407ebe7f794612110057c7d2e
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-03-14 23:47:51 +0530

    Update approach(1).cpp

--- Day 041/approach(1).cpp
+++ Day 041/approach(1).cpp
@@ -1,6 +1,5 @@
 /*
     Title: Set Matrix Zeroes
-
     Problem Statement:
         - Implement a function to modify an M × N matrix such that if an element 
           is 0, its entire row and column are set to 0.
