cdd79a2d3573193cf0301f6e05676b587674c7e6
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-03-24 23:54:25 +0530

    Update approach(1).cpp

--- Day 044/approach(1).cpp
+++ Day 044/approach(1).cpp
@@ -1,6 +1,5 @@
 /*
     Title: Find Triplets with Sum Zero using Brute Force
-
     Problem Statement:
         - Given an array of integers, find all unique triplets whose sum equals zero.
         
