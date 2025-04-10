a26d88ccb5d8b1023d11925da98dbde71fce4fe8
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-04-10 23:36:55 +0530

    Update main.cpp

--- Day 057/main.cpp
+++ Day 057/main.cpp
@@ -2,6 +2,7 @@
     Title: Count Distinct Elements in Every Subarray of Given Size (Sliding Window)
 
     Problem Statement:
+    
         - Given an array of integers and a window size `target`,
           return the count of distinct elements in each subarray of size `target`.
     Approach:
