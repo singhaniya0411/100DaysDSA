2b6b51ee6b5c7ce6d52ac5b791c269862767abfc
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-04-28 23:49:55 +0530

    Update brute-force-approach.cpp

--- Day 062/brute-force-approach.cpp
+++ Day 062/brute-force-approach.cpp
@@ -4,7 +4,6 @@
     Problem Statement:
         - Given an array of integers and an integer k,
           find the length of the longest subarray whose sum equals k.
-
     Approach:
         - For every possible subarray, calculate its sum.
         - If the sum equals k, update the maximum length.
