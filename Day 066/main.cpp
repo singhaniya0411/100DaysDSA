435e88ce366048426c5dca2409407fe34a4f9c07
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-05-04 23:54:03 +0530

    Update main.cpp

--- Day 066/main.cpp
+++ Day 066/main.cpp
@@ -5,7 +5,6 @@
         - Create a singly linked list.
         - Implement functionality to rotate the list to the right by k places.
         - Print the list before and after rotation.
-
     Approach:
         - Count the number of nodes.
         - Use modular arithmetic to avoid full rotations.
