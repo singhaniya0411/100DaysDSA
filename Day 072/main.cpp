b86ca17c50558eb370c336564ded837b603e94bc
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-05-22 23:48:12 -1200

    Update main.cpp

--- Day 072/main.cpp
+++ Day 072/main.cpp
@@ -7,8 +7,9 @@
         - Detect whether a cycle exists.
         - If a cycle exists, find the first node where the cycle begins.
 
+
     Approach:
-        - Use Floyd’s Cycle Detection Algorithm (Tortoise and Hare).
+        - Use Floyd’s Cycle Detection Algorithm .
         - Move two pointers (slow and fast) at different speeds.
         - If they meet, a cycle is present.
         - Reset one pointer to head and move both one step at a time.
