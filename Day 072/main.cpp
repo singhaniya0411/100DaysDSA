4f040fc53cd51571a90ea5b451fbd96c406f9b5e
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-05-22 23:49:14 -1200

    Update main.cpp

--- Day 072/main.cpp
+++ Day 072/main.cpp
@@ -1,7 +1,7 @@
 /*
     Title: Detect Start of Loop in Singly Linked List (Floyd’s Algorithms)
 
-    Problem Statements:
+    Problem Statement:
         - Create a singly linked list.
         - Introduce a cycle manually.
         - Detect whether a cycle exists.
