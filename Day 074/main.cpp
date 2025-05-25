369220ca02006c859a6e81d21578f97c6de4d3bb
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-05-24 22:04:27 -1200

    Update main.cpp

--- Day 074/main.cpp
+++ Day 074/main.cpp
@@ -8,7 +8,8 @@
         - Use an unordered_map to store key -> node mappings for O(1) access.
 
     Time Complexity: 
-        O(1) for both get and put
+        O(1) for both get and put.
+        
     Space Complexity: O(capacity) for storing cache items
 
     Author: Vishal Singhaniya
