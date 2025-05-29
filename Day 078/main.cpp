cb6c709ec92dad411b74b24d2191994924041afe
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-05-29 17:07:54 +0530

    Update main.cpp

--- Day 078/main.cpp
+++ Day 078/main.cpp
@@ -1,7 +1,6 @@
 /*
     Title: Sudoku Solver using Recursion and Backtracking.
 
-
     Problem Statement:
         - Given a 9x9 Sudoku board with some cells filled and others empty (denoted by 0),
         - Fill the board such that each row, column, and 3x3 subgrid contains digits 1 to 9 exactly once.
