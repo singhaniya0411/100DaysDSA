acdeb92f0030cb656b291cfa2bb3790ba8a2a9c9
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-07-03 00:57:15 -1200

    Update main.cpp

--- Day 099/main.cpp
+++ Day 099/main.cpp
@@ -8,6 +8,7 @@
             - Open brackets are closed by the same type of brackets.
             - Open brackets are closed in the correct order.
 
+
     Approach:
         - Use a stack to track opening brackets.
         - Push opening brackets into the stack.
