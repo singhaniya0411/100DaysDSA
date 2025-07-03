39aca06bffdc3853392f37a7cf08a76cfe8bc59e
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-07-03 00:55:04 -1200

    Update main.cpp

--- Day 099/main.cpp
+++ Day 099/main.cpp
@@ -7,6 +7,7 @@
         - A string is valid if:
             - Open brackets are closed by the same type of brackets.
             - Open brackets are closed in the correct order.
+            
 
     Approach:
         - Use a stack to track opening brackets.
