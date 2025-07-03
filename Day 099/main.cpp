7d4622e0cf95941358812d2b326d146c22bb657d
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-07-03 00:54:57 -1200

    Update main.cpp

--- Day 099/main.cpp
+++ Day 099/main.cpp
@@ -5,7 +5,6 @@
         - Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
           determine if the input string has valid (balanced) brackets.
         - A string is valid if:
-        
             - Open brackets are closed by the same type of brackets.
             - Open brackets are closed in the correct order.
 
