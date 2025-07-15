3534d8cc5cd0ade491a0c7df14c09381dd20f8ce
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-07-15 06:08:16 -1200

    Update main.cpp

--- Day 104/main.cpp
+++ Day 104/main.cpp
@@ -8,7 +8,8 @@
 
     Example:
         Input:  [100, 80, 60, 70, 60, 75, 85]
-        Output: [1,   1,  1,  2,  1,  4,  6]
+        Output: [1,   1,  1,  2,  1,  4,  6]'
+        
 
     Approach:
         - Use a stack to keep track of indices of days with prices greater than the current day's price.
