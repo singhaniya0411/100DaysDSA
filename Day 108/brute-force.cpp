9abf682a92a687dad7b816220c32abd9477fd115
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-07-25 20:30:33 -1200

    Update brute-force.cpp

--- Day 108/brute-force.cpp
+++ Day 108/brute-force.cpp
@@ -1,5 +1,5 @@
 /*
-    Title: Maximum of All Subarrays of Size K,
+    Title: Maximum of All Subarrays of Size K
 
     Problem Statement:
         - Given an array of integers and an integer K, find the maximum element 
