3cbb9a121114aa2872dc783d66b259164926cda5
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-07-26 23:46:32 -1200

    Update optimal-approach.cpp

--- Day 108/optimal-approach.cpp
+++ Day 108/optimal-approach.cpp
@@ -1,5 +1,6 @@
 /*
     Title: Maximum of All Subarrays of Size K (Optimized O(n) Solution).
+    
 
     Problem Statement:
         - Given an array of integers and an integer K, find the maximum element 
