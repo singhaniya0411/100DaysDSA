ef99dc561b6e7ebca7bb5addd67565b0087e8eef
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-08-10 20:03:02 -1200

    Add a file via upload

--- Day 115/brute-force.cpp
+++ Day 115/brute-force.cpp
@@ -1,5 +1,5 @@
 /* 
-    Title: Stair Climbing - Count Number of Ways.
+    Title: Stair Climbing - Count Number of Ways
 
     Problem Statement:
         - Given `n` stairs, you can climb either 1 step or 2 steps at a time.
@@ -51,6 +51,7 @@ int main() {
     cout << stairClimbing(stair);
     return 0;
 }
+
 
 
 
