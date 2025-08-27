a9a693d79013bc2c89f227caf332d1d7e5978d49
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-08-27 06:15:15 -1200

    Update main.cpp

--- Day 125/main.cpp
+++ Day 125/main.cpp
@@ -1,5 +1,5 @@
 /* 
-    Title: Maximum Loot from Non-Adjacent Houses (House Robber Problem).
+    Title: Maximum Loot from Non-Adjacent Houses (House Robber Problem)
 
     Problem Statement:
         - You are given an array `arr[]` where each element represents the 
@@ -81,3 +81,4 @@ int main(){
     return 0;
 
 }
+
