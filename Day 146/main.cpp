84f156b43e9234945028414b508129d5e8c77bf2
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-09-19 22:05:11 -1200

    Update main.cpp

--- Day 146/main.cpp
+++ Day 146/main.cpp
@@ -11,7 +11,7 @@
             houses = {{0,0}, {1,3}, {3,3}, {0,3}}
 
         Output:
-            Minimum cost = 8
+            Minimum cost = 6
 
         Explanation:
             - Connect houses (0,0) → (0,3) → (1,3) → (3,3)
