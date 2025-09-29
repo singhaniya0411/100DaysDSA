85078e8bd8921f5edcca14271f9c5db2768988ee
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-09-28 23:52:31 -1200

    Update main.cpp

--- Day 154/main.cpp
+++ Day 154/main.cpp
@@ -1,5 +1,5 @@
 /*
-    Title: Bipartite Graph.
+    Title: Bipartite Graph
 
     Problem Statement:
         - You are given an undirected graph represented by edges.
@@ -114,6 +114,7 @@ int main(){
         cout<<"This graph is not a Bipartite graph\n";
     }
 }
+
 
 
 
