fcbee5d6a5961870b3288a4b2dd789545c13f352
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-10-22 23:36:55 -1200

    Update main.cpp

--- Day 173/main.cpp
+++ Day 173/main.cpp
@@ -1,5 +1,5 @@
 /*
-    Title: Find Lower Bound Index using Binary Search.
+    Title: Find Lower Bound Index using Binary Search
 
     Problem Statement:
         - Given a sorted array `arr` and a target value `target`,
@@ -70,6 +70,7 @@ int main() {
     
     return 0;
 }
+
 
 
 
