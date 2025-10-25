20b9181f70950c47933f6b0fa30a19a30f20e3a1
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-10-24 20:27:52 -1200

    Update main.cpp

--- Day 174/main.cpp
+++ Day 174/main.cpp
@@ -1,5 +1,5 @@
 /*
-    Title: Find Bitonic Point in an Array
+    Title: Find Bitonic Point in an Array.
 
     Problem Statement:
         - A **bitonic array** first increases and then decreases.
@@ -76,3 +76,4 @@ int main() {
 
     return 0;
 }
+
