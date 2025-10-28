037973872117b721cde936f449d5cc37b9b0baa9
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-10-27 23:55:00 -1200

    Update main.cpp

--- Day 177/main.cpp
+++ Day 177/main.cpp
@@ -1,6 +1,5 @@
 /*
-    Title: Minimum Days to Make M Bouquets.
-    
+    Title: Minimum Days to Make M Bouquets
 
     Problem Statement:
         - You are given an array `arr[]` where `arr[i]` represents the day on which the i-th flower blooms.
