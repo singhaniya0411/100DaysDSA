4d577521572fc9a6eb01ba651ec5b9160d787383
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-10-31 23:30:59 -1200

    Update main.cpp

--- Day 181/main.cpp
+++ Day 181/main.cpp
@@ -1,5 +1,5 @@
 /*
-    Title: Generate Matrix from Row and Column Sums
+    Title: Generate Matrix from Row and Column Sums.
 
     Problem Statement:
         - You are given two arrays: `rowSum` (size m) and `colSum` (size n).
@@ -86,5 +86,6 @@ int main() {
     
     return 0;
 }
+
 
 
