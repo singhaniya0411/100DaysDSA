46d37a286fee801e3c7174955b806657b4b76998
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-11-01 23:46:06 -1200

    Update main.cpp

--- Day 182/main.cpp
+++ Day 182/main.cpp
@@ -1,5 +1,6 @@
 /*
     Title: Minimum Operations to Make Matrix Beautiful
+    
 
     Problem Statement:
         - You are given a square matrix `mat` of size n × n.
@@ -92,6 +93,7 @@ int main() {
     
     return 0;
 }
+
 
 
 
