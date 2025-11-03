54c14b0c153ab0d9e506637cd0b19d63c38f3711
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-11-03 06:10:58 -1200

    Update main.cpp

--- Day 183/main.cpp
+++ Day 183/main.cpp
@@ -1,5 +1,5 @@
 /*
-    Title: Triplet Sum in Array.
+    Title: Triplet Sum in Array
 
     Problem Statement:
         - Given an array `arr` of integers and a target value `target`,
@@ -80,6 +80,7 @@ int main() {
     }
     return 0;
 }
+
 
 
 
