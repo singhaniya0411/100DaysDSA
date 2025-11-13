216b9a0324db9508a507bdbbdac4f72ec1914524
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-11-12 23:38:24 -1200

    Update main.cpp

--- Day 192/main.cpp
+++ Day 192/main.cpp
@@ -1,5 +1,5 @@
 /*
-    Title: Count Pairs with Given Difference
+    Title: Count Pairs with Given Difference.
     
     Problem Statement:
         - You are given an integer array `arr` and an integer `k`.
@@ -82,6 +82,7 @@ int main() {
 
     return 0;
 }
+
 
 
 
