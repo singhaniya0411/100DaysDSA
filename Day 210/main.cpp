670c743e9437cec03b4f54992c3030d473fa67e1
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-12-05 22:33:02 -1200

    Update main.cpp

--- Day 210/main.cpp
+++ Day 210/main.cpp
@@ -1,5 +1,5 @@
 /*
-    Title: Check if a Binary Tree is Height Balanced (Optimized Approach)
+    Title: Check if a Binary Tree is Height Balanced (Optimized Approach
 
     Problem Statement:
         - You are given the root of a binary tree.
