ced62b0d4bf36214e884851495841fd956ada3c7
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-12-31 23:46:51 -1200

    Update main.cpp

--- Day 226/main.cpp
+++ Day 226/main.cpp
@@ -1,5 +1,5 @@
 /*
-    Title: Maximum Score from Jumping with K Distance.
+    Title: Maximum Score from Jumping with K Distance
 
     Problem Statement:
         - You are given an integer array `arr` where each element represents a score.
@@ -88,4 +88,5 @@ int main() {
     cout << "Maximum score obtainable: " << getScore(arr, k) << endl;
     return 0;
 }
+
 
